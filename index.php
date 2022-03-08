9c5c9cec12a5a3b04d992f1869c2f27e278f8b52
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-03-08 19:04:57 +0100

    v0.1
    Add custom user agent.
    Add some comments.
    Edit project structure.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -22,35 +22,40 @@
 
         <?php
 
-
-
-
-
-
         require __DIR__ . "/vendor/autoload.php";
         // Import Car class
         require_once './class/Car.php';
 
         // Create a Goutte Client instance
         use Goutte\Client;
+        use Symfony\Component\HttpClient\HttpClient;
 
-        $client = new Client();
+        // Add user agent to get receive correct information
+        // https://github.com/FriendsOfPHP/Goutte/issues/401#issuecomment-591760247
+        $client = new Client(HttpClient::create(array(
+            'headers' => array(
+                'user-agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0', // will be forced using 'Symfony BrowserKit' in executing
+                'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Language' => 'en-US,en;q=0.5',
+                'Referer' => 'http://yourtarget.url/',
+                'Upgrade-Insecure-Requests' => '1',
+                'Save-Data' => 'on',
+                'Pragma' => 'no-cache',
+                'Cache-Control' => 'no-cache',
+            ),
+        )));
+        $client->setServerParameter('HTTP_USER_AGENT', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0');
+
         // Perform GET request to specified url
         $crawler = $client->request('GET', 'https://www.lacentrale.fr/listing');
 
-
-
-
+        // Init empty array to store all Cars
         $cars = array();
 
-
-
-
         // Extract data:
-
+        // Target every single card item
         $crawler->filter('.searchCard__link')->each(function ($node) {
-
-
+            // For each card collect model, version, price, location etc...
             $model = $node->filter('.searchCard__makeModel')->text();
             $version = $node->filter('.searchCard__version')->text();
             $price = $node->filter('.searchCard__fieldPrice')->text();
@@ -58,23 +63,25 @@
             $location = $node->filter('.searchCard__dptCont')->text();
             $km = $node->filter('.searchCard__mileage')->text();
             $year = $node->filter('.searchCard__year')->text();
+            // Collect ad direct link
             $url = "https://www.lacentrale.fr{$node->attr('href')}";
-
+            // Create new Car object to store everything inside it
             $car = new Car($model, $version, $price, $goodDeal, $location, $km, $year, $url);
-
+            // Add created Car object to cars array
             array_push($GLOBALS['cars'], $car);
         });
 
 
 
-
-        include './getLocations.php';
+        // Import getLocations to display chief town close to département number
+        include './utils/getLocations.php';
+        // Array containing all chief town in France
         $locations = getLocations();
 
-
-
+        // Loop through cars array
         foreach ($cars as &$car) {
 
+            // Determine which icon to display depending on how good the deal is
             $dealIcon;
             if (str_contains($car->goodDeal, 'marché')) {
                 $dealIcon = '<i class="fa-solid fa-thumbs-down" style="color:#D40000;"></i>';
@@ -84,7 +91,9 @@
                 $dealIcon = '<i class="fa-solid fa-thumbs-up" style="color:#00aa0e;"></i>';
             }
 
+            // Convert departement number to integer and remove 1 to use the value as index
             $loc = intval($car->location) - 1;
+            // Get the corresponding chief town from locations array
             $city = $locations[$loc];
 
             echo "        
--- utils/getLocations.php
+++ utils/getLocations.php
@@ -4,15 +4,15 @@
 function getLocations()
 {
     // Open file as array
-    $array = file("./dpt.txt");
+    $array = file("./departments.txt");
     // Create new empty array
     $final = array();
 
     // Loop through all values from original array
     foreach ($array as &$value) {
-        // Remove all numbers from every single string
+        // Remove all numbers from every single string to keep names only
         $value = preg_replace('/[0-9]+/', '', $value);
-        // Push value data in final array
+        // Push cleaned value in final array
         array_push($final, $value);
     }
     return $final;
